afac8db5ea81b83d2d94b61104b9331bda233f98
aldopain <jtoming@gmail.com>
2016-10-05 16:53:25 +0300

    Update List.cpp

--- lab1.0/With-Classes/List.cpp
+++ lab1.0/With-Classes/List.cpp
@@ -1,40 +1,51 @@
 #include "header.h"
 using namespace std;
 
+
+
 List::List():name('E'),elements(nullptr){}
 
-List::List(char c):name(c){
-    for(int i = 1; i < N; i++){
-        if(rand()%2){
+void List::add(char c){
+    ListElement a(c, nullptr);
+    this->end->next = &a;
+    end = &a;
+}
 
+List::List(char c):name(c){
+    for(int i = 0; i < 26; i++){
+        if(rand()%2){
+            this->add(i + 97);
         }
     }
 }
 
-List::List(char*){
-
-}
-
-List::List(const List&){
-
-}
-
 List List::operator = (const List&B){
-
+    
 }
 
-List List::operator|(const List&){
-
+List List::operator|(const List&B){
+    List a;   
+    for(ListElement i = head; i; i = i->next)
+        a.add(i.element)
+    for(ListElement j = B->head; j; j = j->next)
+        a.add(i.element)
+    a = a.deleteRepeats(); //написать это
+    return a;
 }
 
-List List::operator&(const List&){
-
+List List::operator&(const List&B){
+    List a;   
+    for(ListElement i = head; i; i = i->next)
+        for(ListElement j = B->head; j; j = j->next)
+            if(i.element == j.element)
+                a.add(i.element)
+    return a;
 }
 
 List List::operator~() const{
-
+                                                                                                            
 }
 
 void List::print(){
-
+    
 }
